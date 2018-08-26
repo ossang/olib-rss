94054d5ae749ecc8ea90ae0f48210e7cf1795083
osh <osh@192.168.0.6>
2018-08-26 19:57:50 +0900

    properties encryption test added

--- olib-rss-rest/olib-rss-rest-server/src/test/java/com/olib/rss/server/OlibRssServerApplicationTests.java
+++ olib-rss-rest/olib-rss-rest-server/src/test/java/com/olib/rss/server/OlibRssServerApplicationTests.java
@@ -1,5 +1,6 @@
 package com.olib.rss.server;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,4 +14,15 @@ public class OlibRssServerApplicationTests {
 	public void contextLoads() {
 	}
 
+	public void encryptionTest() {
+		StandardPBEStringEncryptor jasypt = new StandardPBEStringEncryptor();
+        jasypt.setPassword("olib1234%^&*");      //암호화 키(password)
+        jasypt.setAlgorithm("PBEWithMD5AndDES");
+ 
+        String encryptedText = jasypt.encrypt("sa");    //암호화
+        String plainText = jasypt.decrypt(encryptedText);  //복호화
+ 
+        System.out.println("encryptedText:  " + encryptedText); //암호화된 값
+        System.out.println("plainText:  " + plainText);         //복호화된 값
+	}
 }
